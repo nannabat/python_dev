d3ab9663f0e3091dda5f8a9c1433097fee702e1a
Narendra Annabathula <narendra.33@gmail.com>
2016-11-05 12:12:35 -0700

    code working with diagonal

--- alpha_rangoli.py
+++ alpha_rangoli.py
@@ -9,7 +9,14 @@ alpha_counter = N - 1
 j = hort_tot_len / 2
 
 while vert_counter < vert_up_len:
-	print 'vert_counter:' + str(vert_counter) 
+	print 'vert_counter:' + str(vert_counter)
+	tmp_str = None
 	if vert_counter == 0:
 		print '-'* (hort_tot_len/2) + alpha_list[alpha_counter] + '-' * (hort_tot_len/2)
+	else:
+		j = j - 2
+		lower_limit = (alpha_counter - vert_counter) -  1
+		print '-' * j + alpha_list[alpha_counter]
+
+		
 	vert_counter = vert_counter + 1 
