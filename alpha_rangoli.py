8c99cdb7942a1c347865b3b6febf6fa12f489944
Narendra Annabathula <narendra.33@gmail.com>
2016-11-05 11:27:57 -0700

    working code the prints first line

---
+++ alpha_rangoli.py
@@ -0,0 +1,15 @@
+N = int(raw_input())
+vert_up_len = N
+vert_down_len = N -1
+hort_tot_len = (2 * (N + (N -1))) - 1 
+vert_tot_len = vert_up_len + vert_down_len
+alpha_list = 'abcdefghijklmnopqrstuvwxyz'
+vert_counter = 0
+alpha_counter = N - 1
+j = hort_tot_len / 2
+
+while vert_counter < vert_up_len:
+	print 'vert_counter:' + str(vert_counter) 
+	if vert_counter == 0:
+		print '-'* (hort_tot_len/2) + alpha_list[alpha_counter] + '-' * (hort_tot_len/2)
+	vert_counter = vert_counter + 1 
