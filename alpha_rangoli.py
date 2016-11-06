90a0430a5fc7ed995a05fa14a7712c27e3b229fe
Narendra Annabathula <narendra.33@gmail.com>
2016-11-06 00:18:13 -0700

    code printing two middle terms

--- alpha_rangoli.py
+++ alpha_rangoli.py
@@ -9,15 +9,18 @@ alpha_counter = N - 1
 j = hort_tot_len / 2
 tmp_str_l= alpha_list[alpha_counter]
 tmp_var_l = alpha_counter
+tmp_str_r=alpha_list[alpha_counter - 1]
+tmp_var_r= alpha_counter - 1
 
 while vert_counter < vert_up_len:
-	print 'vert_counter:' + str(vert_counter)
+	#print 'vert_counter:' + str(vert_counter)
 	if vert_counter == 0:
 		print '-'* (hort_tot_len/2) + alpha_list[alpha_counter] + '-' * (hort_tot_len/2)
 	else:
 		j = j - 2
 		tmp_str_l = tmp_str_l + '-' + alpha_list[tmp_var_l]
-		print '-' * j + tmp_str_l
+		tmp_str_r = '-' + tmp_str_r + '-' + alpha_list[tmp_var_r + 1]
+		print '-' * j + tmp_str_l + tmp_str_r + '-' * j
 
 
 		
