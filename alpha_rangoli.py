c8ce9be368164e02791a3b690ebce8654feba4f3
Narendra Annabathula <narendra.33@gmail.com>
2016-11-05 19:51:06 -0700

    working code for upper left part

--- alpha_rangoli.py
+++ alpha_rangoli.py
@@ -7,16 +7,20 @@ alpha_list = 'abcdefghijklmnopqrstuvwxyz'
 vert_counter = 0
 alpha_counter = N - 1
 j = hort_tot_len / 2
+tmp_str_l= alpha_list[alpha_counter]
+tmp_var_l = alpha_counter
 
 while vert_counter < vert_up_len:
 	print 'vert_counter:' + str(vert_counter)
-	tmp_str = None
 	if vert_counter == 0:
 		print '-'* (hort_tot_len/2) + alpha_list[alpha_counter] + '-' * (hort_tot_len/2)
 	else:
 		j = j - 2
-		lower_limit = (alpha_counter - vert_counter) -  1
-		print '-' * j + alpha_list[alpha_counter]
+		tmp_str_l = tmp_str_l + '-' + alpha_list[tmp_var_l]
+		print '-' * j + tmp_str_l
+
 
 		
-	vert_counter = vert_counter + 1 
+	vert_counter = vert_counter + 1
+	tmp_var_l = tmp_var_l - 1 
+ 
